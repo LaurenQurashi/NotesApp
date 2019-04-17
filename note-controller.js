838d03e27ae43993d506dda92a33b545f395fc34
Lauren <laurenqurashi@me.com>
2019-04-17 15:36:23 +0100

    Refactored the note-controller to include an actual note rather than inputted text

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,14 @@
 window.onload = function() {
-  console.log(document.getElementById('app').innerHTML = "howdy")
+  console.log(document.getElementById('app').innerHTML = nVL.htmlMaker(notelist))
 };
+
+// You'll need to instantiate your note list model.
+var notelist = new NoteList;
+
+// You'll need to add a note to it.
+notelist.store(new Note("Noote"));
+// You'll need to use your note list view to convert your note list model to HTML.
+nVL = new NotesListView;
+nVL.htmlMaker(notelist);
+
+// You'll need to dynamically insert that HTML into the body of your index.html page.
